da92f65eebd1c0cce0fbad6f9dc5ea22c749f355
BlueJay <flynntaylor1409@gmail.com>
2025-09-30 19:59:23 +0100

    Added comments

--- Assets/DES402/Player/PlayerController.cs
+++ Assets/DES402/Player/PlayerController.cs
@@ -5,10 +5,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InstanceManager intanceManager;
     [SerializeField] private int myInstanceNumber;
-    [SerializeField] private float speed;
-    private float gravity = -9.8f;
 
     private Rigidbody2D myRigidBody;
+
+    //the button the player needs to press to move
     private WhaleButton expectedInput = WhaleButton.L_Button;
 
     private void Awake()
@@ -19,21 +19,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
-
-
+        // if the player presses the correct button move them
         if (PlayerGaveExpectedInput())
         {
 
-
-            //transform.Translate(Vector2.right * speed * Time.deltaTime);
-
             Vector2 finalForce = Vector2.right * 1000f * Time.deltaTime;
 
+            //reset velocity to prevent spamming
             myRigidBody.linearVelocity = Vector2.zero;
+
+            //move the player
             myRigidBody.AddForce(finalForce, ForceMode2D.Impulse);
 
+            //update the expected input
             expectedInput = UpdateExpectedInput();
         }
 
@@ -41,6 +39,7 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    //checks if the player pressed the correct button to move
     private bool PlayerGaveExpectedInput()
     {
         switch (expectedInput) 
@@ -54,8 +53,11 @@ public class PlayerController : MonoBehaviour
 
         return false;
     }
+
+    //place holder, just switches the cuurrent expected button to the other button
     private WhaleButton UpdateExpectedInput()
     {
+        //TODO: replace this with a timer that alternates between the expected buttons
         switch(expectedInput)
         {
             case WhaleButton.L_Button:
